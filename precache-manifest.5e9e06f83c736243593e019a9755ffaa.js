77e860e12dd08cdd9b0e5573594367ff50eb5f8e
nvoevodin <46409820+nvoevodin@users.noreply.github.com>
2020-10-06 13:46:03 -0400

    Add files via upload

---
+++ precache-manifest.5e9e06f83c736243593e019a9755ffaa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea430cdf25bfe1a0ebcdf8282f89ea5e",
+    "url": "/nvoevodin/VolleyPal/index.html"
+  },
+  {
+    "revision": "7064b8ed8724161638a5",
+    "url": "/nvoevodin/VolleyPal/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "42ad87bf80ed4f8f2a8e",
+    "url": "/nvoevodin/VolleyPal/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7064b8ed8724161638a5",
+    "url": "/nvoevodin/VolleyPal/static/js/2.068b99d0.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/nvoevodin/VolleyPal/static/js/2.068b99d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ad87bf80ed4f8f2a8e",
+    "url": "/nvoevodin/VolleyPal/static/js/main.0c717c01.chunk.js"
+  },
+  {
+    "revision": "99ac9ab9795e4ff8d768",
+    "url": "/nvoevodin/VolleyPal/static/js/runtime-main.b99541eb.js"
+  },
+  {
+    "revision": "4690cb380a9be406657318a3896db27e",
+    "url": "/nvoevodin/VolleyPal/static/media/Volleyball.4690cb38.jpg"
+  },
+  {
+    "revision": "d11ea3fa51d8e98c8d9664e6e21b7cb9",
+    "url": "/nvoevodin/VolleyPal/static/media/androidQR.d11ea3fa.png"
+  },
+  {
+    "revision": "1d32c3733bc213ad52ab5556ffce7f82",
+    "url": "/nvoevodin/VolleyPal/static/media/iosQR.1d32c373.png"
+  }
+]);
